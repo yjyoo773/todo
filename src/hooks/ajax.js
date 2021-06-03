e97083707745021cd08759c0f1df68b691c33e4f
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-06-03 12:22:31 -0700

    clean up code and add engine in package.json

--- src/hooks/ajax.js
+++ src/hooks/ajax.js
@@ -4,34 +4,40 @@ const useAjax = (list) => {
   const todoAPI = "https://ellis-api-server.herokuapp.com/todo";
 
   const handleGet = async (action) => {
-    let getList = await axios.get(todoAPI);
-    let data = getList.data;
-    action(data);
-  };
-
-  const handlePost = async (item, action) => {
-    item.due = new Date();
-    item.complete = false;
-
     try {
-      let newItem = await axios.post(todoAPI, item);
-      action(newItem.data);
+      let getList = await axios.get(todoAPI);
+      let data = getList.data;
+      action(data);
     } catch (e) {
       console.error(e);
     }
   };
 
+  const handlePost = async (item, action) => {
+      
+    try {
+
+      item.due = new Date();
+      item.complete = false;
+      let newItem = await axios.post(todoAPI, item);
+      action(newItem.data);
+      
+    } catch (e) {
+        
+      console.error(e);
+    }
+  };
+
   const handlePut = async (id, action) => {
+
     try {
       let item = list.filter((i) => i._id === id)[0] || {};
       if (item._id) {
         item.complete = !item.complete;
         let updatedItem = await axios.put(`${todoAPI}/${id}`, item);
-        let data = updatedItem.data
+        let data = updatedItem.data;
         action(
-          list.map((listItem) =>
-            listItem._id === data._id ? data : listItem
-          )
+          list.map((listItem) => (listItem._id === data._id ? data : listItem))
         );
       }
     } catch (e) {
@@ -51,4 +57,5 @@ const useAjax = (list) => {
 
   return [handleGet, handlePost, handlePut, handleDelete];
 };
+
 export default useAjax;
